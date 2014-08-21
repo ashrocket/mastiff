c6f35c8cab7e551553413b975dca0f6cc58ac7c6
ashrocket <ashley@raiteri.net>
2014-08-21 15:33:45 -0400

    Updated attachment dir configuration

--- lib/generators/mastiff/install/templates/sidekiq/sync_mail_worker.rb
+++ lib/generators/mastiff/install/templates/sidekiq/sync_mail_worker.rb
@@ -5,13 +5,18 @@ class SyncMailWorker
   include Sidekiq::Lock::Worker
   include Sidetiq::Schedulable
   sidekiq_options :queue => :email_queue, :retry => false, :backtrace => true
-  sidekiq_options lock: { timeout: 1000, name: 'lock-worker' }
+  sidekiq_options lock: { timeout: 120000, name: 'lock-mail-worker' }
 
   recurrence { minutely }
 
   def perform
-    if lock.acquire!
-      Mastiff::Email.sync_messages
-    end
+      if lock.acquire!
+        begin
+          Mastiff::Email.sync_messages
+        ensure
+          lock.release!
+        end
+      end
   end
+  
 end
